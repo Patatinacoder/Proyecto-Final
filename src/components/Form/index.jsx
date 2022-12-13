bac1e54dfaface5e18c134d8b0dd62a243e2b008
Patatinacoder <105814556+Patatinacoder@users.noreply.github.com>
2022-12-13 18:18:46 -0300

    form Listo

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,56 +1,108 @@
-import { useState } from "react";
+// import { useState } from "react";
+
+import { useForm } from "react-hook-form"
 
 
-const Form = () =>{
+// const Form = () =>{
 
 
-    const [name, setName]= useState ('')
-    const [email, setEmail] = useState('')
-    return (
- <form onSubmit={ev=> {
-    ev.preventDefault()
+//     const [name, setName]= useState ('')
+//     const [email, setEmail] = useState('')
+//     return (
+//  <form onSubmit={ev=> {
+//     ev.preventDefault()
 
-    const name = ev.target.name.value
-    const email = ev.target.email.value
+//     const name = ev.target.name.value
+//     const email = ev.target.email.value
 
-    login(name, email)
+//     login(name, email)
 
- }}>
+//  }}>
  
- <input 
- type="text"
-  name='name' 
-  placeholder="Name"
-  value={name}
-  onChange={ev => setName(ev.target.value)}
-  autoComplete= 'off' 
+//  <input 
+//  type="text"
+//   name='name' 
+//   placeholder="Name"
+//   value={name}
+//   onChange={ev => setName(ev.target.value)}
+//   autoComplete= 'off' 
   
-  ></input> 
+//   ></input> 
 
-  <input type="text"
-  name="email"
-  placeholder="Email"
-  value={email} 
-  onChange={ev=> setEmail(ev.target.value)}
-  autoComplete="off"
-  ></input>
+//   <input type="text"
+//   name="email"
+//   placeholder="Email"
+//   value={email} 
+//   onChange={ev=> setEmail(ev.target.value)}
+//   autoComplete="off"
+//   ></input>
 
  
 
 
-<button type="submit">Comprar</button>
+// <button type="submit">Comprar</button>
 
- </form>
+//  </form>
 
-    )
+//     )
     
- } 
+//  } 
 
- const login = (name, email) => {
-    if (name === "" && email==="")
- alert ('login correcto')
- else{
-    alert('login incorrecto')
- }
+//  const login = (name, email) => {
+//     if (name === "" && email==="")
+//  alert ('login correcto')
+//  else{
+//     alert('login incorrecto')
+//  }
+// }
+//  export default Form
+
+
+
+const Formulario = () =>{
+    const {register,formState:{errors}, watch,  handleSubmit} = useForm()
+    })
+
+    const onSubmit = (data) =>
+{
+    console.log(data);
 }
- export default Form
+
+const incluirTelefono = watch('incluirTelefono')
+    return <div>
+
+        <h2>Form</h2>
+    <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label >Name</label>
+                <input type="text" {...register('nombre', {
+                    required:true})} />
+                    {errors.name?.type==='required' && <p>Required</p>}
+
+                <div>
+                    <label>Email</label>
+                    <input type="text" {...register('email',{
+                        required:true,
+                        pattern:/^[^\s@]+@[^\s@]+\.[^\s@]+$/i
+                    })} />
+                    {errors.email?.type=== 'pattern' && <p>Incorrect format</p>}
+                </div>
+
+                <div>
+                    <label>Include Phone Number?</label>
+                    <input type="checkbox" {...register('Include Phone')}/>
+
+                </div>
+                {incluirTelefono && (
+                <div>
+                <label>Phone</label>
+                <input type="text" {...register('phone')} />
+                </div>
+                )}
+            </div>
+<input type="submit" value="Enviar" />
+        </form>
+    </div>
+}
+
+export default Formulario
